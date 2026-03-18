7213ad2a603e7812a7b723912743282de0e66407
mjun6356 <mjun6356@g.shingu.ac.kr>
2026-03-18 14:42:57 +0900

    bb

--- Assets/player Controller.cs
+++ Assets/player Controller.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
+    private Vector2 moveInput;
+    public float moveSpeed = 7.0f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
     }
 }
